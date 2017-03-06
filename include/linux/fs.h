2f7908fdff578b2ad5ead7a36b4dd328994de41c
DespairFactor <matthewalex@linux.com>
2017-03-06 16:53:10 -0500

    Revert f2s changes to get closer to mainline

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -46,8 +46,6 @@ struct vfsmount;
 struct cred;
 struct swap_info_struct;
 struct seq_file;
-struct fscrypt_info;
-struct fscrypt_operations;
 struct workqueue_struct;
 struct fscrypt_info;
 struct fscrypt_operations;
